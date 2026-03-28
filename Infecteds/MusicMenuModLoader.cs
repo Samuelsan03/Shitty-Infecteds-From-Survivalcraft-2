56bfdedf90b920cd55f0d116a459a6ad21665e59
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-03-27 21:43:10 -0500

    Update MusicMenuModLoader.cs

--- Infecteds/MusicMenuModLoader.cs
+++ Infecteds/MusicMenuModLoader.cs
@@ -1,21 +1,28 @@
+using System;
+using System.Collections.Generic;
 using Game;
 
-public class MusicMenuModLoader : ModLoader
+public class MenuMusicModLoader : ModLoader
 {
+	// Lista de rutas de música (sin extensión) dentro del mod
+	private static readonly List<string> ListaMusica = new List<string>
+	{
+		"Music/Menu Music",
+		"Music/Menu Music 2",
+	};
+
+	private Game.Random random = new Game.Random(); // Asegúrate de usar System.Random o el Random del juego
+
 	public override void __ModInitialize()
 	{
-		// Register our mod loader for the "MenuPlayMusic" hook.
-		// Priority: lower values execute first. We want our music to be used if no other mod overrides it,
-		// but we can set a default priority. Other mods may also set their own music; we don't want to block them,
-		// so we'll use a neutral priority.
-		ModsManager.RegisterHook("MenuPlayMusic", this, 0);
+		// Registrar el hook con prioridad por defecto (0)
+		ModsManager.RegisterHook("MenuPlayMusic", this);
 	}
 
 	public override void MenuPlayMusic(out string contentMusicPath)
 	{
-		// Provide the path to our custom menu music.
-		// The file should be placed in the mod's content folder under "Music/Menu Music".
-		// The game's ContentManager will resolve the path relative to the mod's content.
-		contentMusicPath = "Music/Menu Music";
+		// Seleccionar una canción aleatoria de la lista
+		int index = random.Int(ListaMusica.Count);
+		contentMusicPath = ListaMusica[index];
 	}
 }
