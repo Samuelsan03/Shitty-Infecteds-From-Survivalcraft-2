44bf46f8b8b50edf3a19a51035f0e7e470d98339
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-10 15:20:32 -0500

    Add ShittyInfectedsSettings class for game settings

---
+++ Infecteds/ShittyInfectedsSettings.cs
@@ -0,0 +1,10 @@
+namespace Game
+{
+	public static class ShittyInfectedsSettings
+	{
+		public static bool EnableCreatureAttacks { get; set; } = true;
+		public static bool AttackOnHitCreative { get; set; } = true;
+
+		// Futuras configuraciones se agregarán aquí
+	}
+}
