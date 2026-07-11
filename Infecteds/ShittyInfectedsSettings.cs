9448f7e6ccf614f640c887bab54cb7b8e38d1eff
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-11 13:03:25 -0500

    Add ShowCoordinates setting to ShittyInfectedsSettings

--- Infecteds/ShittyInfectedsSettings.cs
+++ Infecteds/ShittyInfectedsSettings.cs
@@ -4,6 +4,7 @@ namespace Game
 	{
 		public static bool EnableCreatureAttacks { get; set; } = true;
 		public static bool AttackOnHitCreative { get; set; } = true;
+		public static bool ShowCoordinates = true;
 
 		// Futuras configuraciones se agregarán aquí
 	}
