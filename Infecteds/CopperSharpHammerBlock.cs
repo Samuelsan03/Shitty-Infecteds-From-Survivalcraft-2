c51e5c694ab065bfc1ec33457d67f87c69704fb2
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-12 12:09:20 -0500

    Create CopperSharpHammerBlock.cs

---
+++ Infecteds/CopperSharpHammerBlock.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Game
+{
+	// Token: 0x02000043 RID: 67
+	public class CopperSharpHammerBlock : SharpHammerBlock
+	{
+		// Token: 0x06000261 RID: 609 RVA: 0x0000D132 File Offset: 0x0000B332
+		public CopperSharpHammerBlock() : base(47, 79)
+		{
+		}
+
+		// Token: 0x04000129 RID: 297
+		public static int Index = 502;
+	}
+}
