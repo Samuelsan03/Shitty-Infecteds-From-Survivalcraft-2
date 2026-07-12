0ef6966412ea29d23c0e55794a9f28a071ed3135
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-12 16:48:23 -0500

    Update Shitty Infecteds Recipes.cr

--- Shitty Infecteds Recipes.cr
+++ Shitty Infecteds Recipes.cr
@@ -1,4 +1,34 @@
 <Recipes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="CraftingRecipes.xsd">
+    <Recipe Result="WoodenLongspearBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" c="woodenspear" Description="[0]">
+      " b"
+      "bcb"
+      " a"
+    </Recipe>
+    <Recipe Result="StoneLongspearBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" c="stonespear" Description="[0]">
+      " b"
+      "bcb"
+      " a"
+    </Recipe>
+    <Recipe Result="StoneLongspearBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="stonechunk" c="stonespear" Description="[1]">
+      " b"
+      "bcb"
+      " a"
+    </Recipe>
+    <Recipe Result="CopperLongspearBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="2" a="stick" b="copperingot" c="copperspear" Description="[0]">
+      " b"
+      "bcb"
+      " a"
+    </Recipe>
+    <Recipe Result="IronLongspearBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="3" a="stick" b="ironingot" c="ironspear" Description="[0]">
+      " b"
+      "bcb"
+      " a"
+    </Recipe>
+    <Recipe Result="DiamondLongspearBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="2" a="stick" b="diamond" c="diamondspear" Description="[0]">
+      " b"
+      "bcb"
+      " a"
+    </Recipe>
     <Recipe Result="CopperSharpHammerBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="2" a="stick" b="copperingot" Description="[0]">
       " bb"
       "bab"
@@ -15,7 +45,10 @@
       " a "
     </Recipe>
     <Smelting>
-    <Recipe Result="LavaSharpHammerBlock" ResultCount="1" RequiredHeatLevel="2" a="magmabucket" b="diamondsharphammer" Description="[0]">
+    <Recipe Result="LavaSharpHammerBlock" ResultCount="1" RequiredHeatLevel="2" RequiredPlayerLevel="5" a="magmabucket" b="diamondsharphammer" Description="[0]">
+      "ba"
+    </Recipe>
+    <Recipe Result="LavaLongspearBlock" ResultCount="1" RequiredPlayerLevel="5" RequiredHeatLevel="2" a="magmabucket" b="diamondlongspear" Description="[0]">
       "ba"
     </Recipe>
     </Smelting>
