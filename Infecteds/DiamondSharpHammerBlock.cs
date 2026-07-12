08154f8123fedd5de6079159e575311326b7df8a
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-12 12:09:46 -0500

    Create DiamondSharpHammerBlock.cs

---
+++ Infecteds/DiamondSharpHammerBlock.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Game
+{
+	// Token: 0x02000056 RID: 86
+	public class DiamondSharpHammerBlock : SharpHammerBlock
+	{
+		// Token: 0x060002C3 RID: 707 RVA: 0x0000E72E File Offset: 0x0000C92E
+		public DiamondSharpHammerBlock() : base(47, 182)
+		{
+		}
+
+		// Token: 0x04000156 RID: 342
+		public static int Index = 504;
+	}
+}
