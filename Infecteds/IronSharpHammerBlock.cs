b9b98382380632a3102decfb1ad8ee8b9d48025d
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-12 12:09:36 -0500

    Create IronSharpHammerBlock.cs

---
+++ Infecteds/IronSharpHammerBlock.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Game
+{
+	// Token: 0x02000092 RID: 146
+	public class IronSharpHammerBlock : SharpHammerBlock
+	{
+		// Token: 0x06000448 RID: 1096 RVA: 0x000154BC File Offset: 0x000136BC
+		public IronSharpHammerBlock() : base(47, 63)
+		{
+		}
+
+		// Token: 0x040001D9 RID: 473
+		public static int Index = 503;
+	}
+}
