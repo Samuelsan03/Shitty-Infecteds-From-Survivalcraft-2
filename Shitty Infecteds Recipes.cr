9e8a42e547569e5192474b876779fb9164be1901
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-12 12:31:58 -0500

    Create Shitty Infecteds Recipes.cr

---
+++ Shitty Infecteds Recipes.cr
@@ -0,0 +1,22 @@
+<Recipes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="CraftingRecipes.xsd">
+    <Recipe Result="CopperSharpHammerBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="2" a="stick" b="copperingot" Description="[0]">
+      " bb"
+      "bab"
+      " a "
+    </Recipe>
+    <Recipe Result="IronSharpHammerBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="3" a="stick" b="ironingot" Description="[0]">
+      " bb"
+      "bab"
+      " a "
+    </Recipe>
+    <Recipe Result="DiamondSharpHammerBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="4" a="stick" b="diamond" Description="[0]">
+      " bb"
+      "bab"
+      " a "
+    </Recipe>
+    <Smelting>
+    <Recipe Result="LavaSharpHammerBlock" ResultCount="1" RequiredHeatLevel="2" a="magmabucket" b="diamondsharphammer" Description="[0]">
+      "ba"
+    </Recipe>
+    </Smelting>
+</Recipes>
