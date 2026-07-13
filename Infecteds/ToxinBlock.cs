ad458350d12bae8e3c2a0f7c1be27661ab626a50
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-13 12:17:01 -0500

    Create ToxinBlock.cs

---
+++ Infecteds/ToxinBlock.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+	public class ToxinBlock : ShittyInfectedsFlatBlock
+	{
+		public ToxinBlock() : base("Textures/toxina")
+		{
+		}
+
+		public const int Index = 512;
+	}
+}
