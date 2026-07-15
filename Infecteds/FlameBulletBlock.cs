4a613839c33e61081dcce52e5169e35b5fbb6467
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-15 13:14:30 -0500

    Add Poison bullet type to FlameBulletBlock
    Added Poison bullet type with corresponding properties and methods.

--- Infecteds/FlameBulletBlock.cs
+++ Infecteds/FlameBulletBlock.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Engine;
 using Engine.Graphics;
@@ -9,45 +9,41 @@ namespace Game
 	{
 		public static int Index = 514;
 
-		// Enum sin números explícitos
 		public enum FlameBulletType
 		{
-			Fire
+			Fire,
+			Poison
 		}
 
-		private static readonly float[] m_sizes = new float[] { 1f };
-		private static readonly int[] m_textureSlots = new int[] { 229 };
-		private static readonly Color[] m_colors = new Color[] { new Color(255, 128, 0) };
+		private static readonly float[] m_sizes = new float[] { 1f, 1f };
+		private static readonly int[] m_textureSlots = new int[] { 229, 229 };
+		private static readonly Color[] m_colors = new Color[] { new Color(255, 128, 0), Color.Green };
+		private static readonly float[] m_weaponPowers = new float[] { 1f, 1f }; // Daño moderado
 
-		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
-		{
-		}
+		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z) { }
 
 		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
 		{
 			int type = (int)GetBulletType(Terrain.ExtractData(value));
 			float size2 = (type >= 0 && type < m_sizes.Length) ? (size * m_sizes[type]) : size;
 			Color bulletColor = (type >= 0 && type < m_colors.Length) ? m_colors[type] : Color.White;
-
 			BlocksManager.DrawFlatOrImageExtrusionBlock(primitivesRenderer, value, size2, ref matrix, null, bulletColor, false, environmentData);
 		}
 
 		public override float GetProjectilePower(int value)
 		{
+			int type = (int)GetBulletType(Terrain.ExtractData(value));
+			if (type >= 0 && type < m_weaponPowers.Length)
+				return m_weaponPowers[type];
 			return 0f;
 		}
 
-		public override float GetExplosionPressure(int value)
-		{
-			return 0f;
-		}
+		public override float GetExplosionPressure(int value) => 0f;
 
 		public override IEnumerable<int> GetCreativeValues()
 		{
 			foreach (FlameBulletType type in Enum.GetValues(typeof(FlameBulletType)))
-			{
 				yield return Terrain.MakeBlockValue(Index, 0, SetBulletType(0, type));
-			}
 		}
 
 		public override string GetDisplayName(SubsystemTerrain subsystemTerrain, int value)
@@ -55,7 +51,7 @@ namespace Game
 			int type = (int)GetBulletType(Terrain.ExtractData(value));
 			if (type < 0 || type >= Enum.GetValues<FlameBulletType>().Length)
 				return string.Empty;
-			return "Flame Bullet";
+			return type == 0 ? "Flame Bullet" : "Poison Bullet";
 		}
 
 		public override int GetFaceTextureSlot(int face, int value)
@@ -66,14 +62,7 @@ namespace Game
 			return m_textureSlots[type];
 		}
 
-		public static FlameBulletType GetBulletType(int data)
-		{
-			return (FlameBulletType)(data & 15);
-		}
-
-		public static int SetBulletType(int data, FlameBulletType bulletType)
-		{
-			return (data & -16) | (int)(bulletType & (FlameBulletType)15);
-		}
+		public static FlameBulletType GetBulletType(int data) => (FlameBulletType)(data & 15);
+		public static int SetBulletType(int data, FlameBulletType bulletType) => (data & -16) | (int)(bulletType & (FlameBulletType)15);
 	}
 }
