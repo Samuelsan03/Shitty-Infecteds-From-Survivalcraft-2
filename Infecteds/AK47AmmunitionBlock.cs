90f827c3af00d6df85915b28d759e71c4b0dd1be
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-07 12:15:38 -0500

    Add AK47AmmunitionBlock class for AK47 ammunition

---
+++ Infecteds/AK47AmmunitionBlock.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+	public class AK47AmmunitionBlock : ShittyInfectedsFlatBlock
+	{
+		public AK47AmmunitionBlock() : base("Textures/Armas De Mierda/AK47Bullet")
+		{
+		}
+
+		public const int Index = 1001;
+	}
+}
