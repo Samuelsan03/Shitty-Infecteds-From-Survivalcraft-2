6385f433d9015af42c1cce32b427ecddea6b7665
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-09 19:16:47 -0500

    Add files via upload

---
+++ Infecteds/FrozenSnowballBlock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Engine;
+using Engine.Graphics;
+
+namespace Game
+{
+	public class FrozenSnowballBlock : Block
+	{
+		public static int Index = 519;
+
+		public BlockMesh m_standaloneBlockMesh = new BlockMesh();
+
+		public override void Initialize()
+		{
+			Model model = ContentManager.Get<Model>("Models/Snowball");
+			Matrix boneAbsoluteTransform = BlockMesh.GetBoneAbsoluteTransform(model.FindMesh("Snowball", true).ParentBone);
+			this.m_standaloneBlockMesh.AppendModelMeshPart(model.FindMesh("Snowball", true).MeshParts[0], boneAbsoluteTransform * Matrix.CreateTranslation(0f, 0f, 0f), false, false, false, false, new Color(140, 200, 255));
+			base.Initialize();
+		}
+
+		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
+		{
+		}
+
+		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
+		{
+			Texture2D defaultTexture = this.GetDefaultTexture(value);
+			if (defaultTexture == null)
+			{
+				BlocksManager.DrawMeshBlock(primitivesRenderer, this.m_standaloneBlockMesh, color, 2.5f * size, ref matrix, environmentData);
+				return;
+			}
+			BlocksManager.DrawMeshBlock(primitivesRenderer, this.m_standaloneBlockMesh, defaultTexture, color, 2.5f * size, ref matrix, environmentData);
+		}
+	}
+}
---
+++ Infecteds/SubsystemFrozenSnowballBlockBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Engine;
+using TemplatesDatabase;
+
+namespace Game
+{
+	public class SubsystemFrozenSnowballBlockBehavior : SubsystemBlockBehavior
+	{
+		public override int[] HandledBlocks
+		{
+			get
+			{
+				return new int[] { FrozenSnowballBlock.Index };
+			}
+		}
+
+		public override void OnFiredAsProjectile(Projectile projectile)
+		{
+			this.m_subsystemProjectiles.AddTrail(projectile, Vector3.Zero, new FrozenSmokeTrailParticleSystem(20, 1f, float.MaxValue, Color.White));
+			projectile.ProjectileStoppedAction = ProjectileStoppedAction.Disappear;
+		}
+
+		public override bool OnHitAsProjectile(CellFace? cellFace, ComponentBody componentBody, WorldItem worldItem)
+		{
+			this.m_subsystemAudio.PlaySound("Audio/congelado", 1f, 0f, worldItem.Position, 3f, false);
+			if (componentBody != null && componentBody.Entity != null)
+			{
+				ComponentPlayer componentPlayer = componentBody.Entity.FindComponent<ComponentPlayer>();
+				if (componentPlayer != null && componentPlayer.ComponentVitalStats != null)
+				{
+					componentPlayer.ComponentVitalStats.Temperature = Math.Max(0f, componentPlayer.ComponentVitalStats.Temperature - 4f);
+					componentPlayer.ComponentVitalStats.Wetness = Math.Min(1f, componentPlayer.ComponentVitalStats.Wetness + 0.3f);
+					if (componentPlayer.ComponentFlu != null)
+					{
+						componentPlayer.ComponentFlu.StartFlu();
+					}
+				}
+				else
+				{
+					ComponentCreatureFlu componentCreatureFlu = componentBody.Entity.FindComponent<ComponentCreatureFlu>();
+					if (componentCreatureFlu != null)
+					{
+						componentCreatureFlu.ForceInfect(600f);
+					}
+					else
+					{
+						ComponentFlu componentFlu = componentBody.Entity.FindComponent<ComponentFlu>();
+						if (componentFlu != null)
+						{
+							componentFlu.StartFlu();
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		public override void Load(ValuesDictionary valuesDictionary)
+		{
+			base.Load(valuesDictionary);
+			this.m_subsystemProjectiles = base.Project.FindSubsystem<SubsystemProjectiles>(true);
+			this.m_subsystemAudio = base.Project.FindSubsystem<SubsystemAudio>(true);
+		}
+
+		public SubsystemProjectiles m_subsystemProjectiles;
+
+		public SubsystemAudio m_subsystemAudio;
+	}
+}
