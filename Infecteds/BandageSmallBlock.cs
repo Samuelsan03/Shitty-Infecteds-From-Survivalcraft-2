a665322780b9d02f8023a0175ac8aaa87a718814
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-11 20:20:26 -0500

    Add BandageSmallBlock class for small bandage item

---
+++ Infecteds/BandageSmallBlock.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+	public class BandageSmallBlock : ShittyInfectedsFlatBlock
+	{
+		public BandageSmallBlock() : base("Textures/bendaje small")
+		{
+		}
+
+		public const int Index = 520;
+	}
+}
