7f432c9e52f5fb6d5666671c958a684606409e63
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-19 13:03:31 -0500

    Add LargeBandageBlock class for game mechanics

---
+++ Infecteds/LargeBandageBlock.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+	public class LargeBandageBlock : ShittyInfectedsFlatBlock
+	{
+		public LargeBandageBlock() : base("Textures/bendaje")
+		{
+		}
+
+		public const int Index = 521;
+	}
+}
