afc0868c40178eda9282c2bec7f95b222cca5828
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-19 23:01:33 -0500

    Create AntidotePillBlock.cs

---
+++ Infecteds/AntidotePillBlock.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+	public class AntidotePillBlock : ShittyInfectedsFlatBlock
+	{
+		public AntidotePillBlock() : base("Textures/pastilla")
+		{
+		}
+
+		public const int Index = 522;
+	}
+}
