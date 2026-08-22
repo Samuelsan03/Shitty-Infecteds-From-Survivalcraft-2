ac5638fe389331ad0f5395305b5359b01e70f8d2
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-21 22:17:10 -0500

    Create CoinBlock.cs

---
+++ Infecteds/CoinBlock.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+	public class CoinBlock : ShittyInfectedsFlatBlock
+	{
+		public CoinBlock() : base("Textures/moneda colombiana")
+		{
+		}
+
+		public const int Index = 523;
+	}
+}
